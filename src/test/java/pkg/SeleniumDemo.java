e5f0e5e34adcad2910d5d3772dcfbbcda429245c
kiranreddy365 <kiranreddy365@gmail.com>
2019-12-05 17:49:35 +0530

    Java Project checkin

---
+++ src/test/java/pkg/SeleniumDemo.java
@@ -0,0 +1,50 @@
+package pkg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SeleniumDemo {
+	
+	WebDriver driver;
+	String URL="https://www.google.com/";
+	
+	@BeforeClass
+	public void setup()
+	{
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get(URL);
+		System.out.println("Browser lunched.");
+	}
+	
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("Test1 executed.");
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("Test2 executed.");
+	}
+	
+	@Test
+	public void test3()
+	{
+		System.out.println("Test3 executed.");
+	}
+	
+	@AfterClass
+	public void tearDown()
+	{
+		driver.quit();
+		System.out.println("Browser closed.");
+	}
+	
+
+}
